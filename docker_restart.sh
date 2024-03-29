dff616520641f05465ee81ca55f8ebeba78c8109
Zohaib Tariq <se.zohaib@gmail.com>
2024-03-29 21:46:50 +0500

    docker restart check service

---
+++ docker_restart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if Docker containers are running, if not, start them
+docker_compose_file="/root/pilgrim-staging-backend-docker/docker-compose.yml"
+docker_compose_service="laravel mysql redis"
+
+for service in $docker_compose_service; do
+    if ! docker-compose -f $docker_compose_file ps -q $service >/dev/null; then
+        echo "Container $service is not running. Starting it now..."
+        docker-compose -f $docker_compose_file up -d $service
+    fi
+done
+
+#sudo systemctl enable docker_restart.service
+#sudo systemctl start docker_restart.service
